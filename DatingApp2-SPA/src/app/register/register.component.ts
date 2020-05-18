bd106d8ea244dbfb0a05404c20e92518a4d5d9bc
Diego <diegotre07@gmail.com>
2020-05-18 21:46:26 +0200

    Fin Seccion 5

--- DatingApp2-SPA/src/app/register/register.component.ts
+++ DatingApp2-SPA/src/app/register/register.component.ts
@@ -19,7 +19,7 @@ export class RegisterComponent implements OnInit {
   register() {
     this.authService.register(this.model).subscribe(() => {
       console.log('Registo completado');
-    }, error =>{
+    }, error => {
       console.log(error);
     });
   }
